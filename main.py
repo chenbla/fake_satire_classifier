df5bc9f5450586589f723cd5b0d4a6b30d3f36a5
Chen <chenbla@gmail.com>
2022-12-04 19:55:22 +0200

    fix procfile

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ import pickle
 
 from flask_handler import run_flask
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--train_mode', action='store_true')
-    parser.add_argument('--backbone', default='all-mpnet-base-v2')
-    parser.add_argument('--dont_use_headlines',  action='store_false')
-    args = parser.parse_args()
 
-    if args.train_mode:
+def main(train_mode=False, backbone='all-mpnet-base-v2', dont_use_headlines=True):
+    if train_mode:
         # read the data
         print("reading the data")
         dataset_path = Path(os.path.dirname(__file__)) / "FakeNewsData" / "FakeNewsData" / "StoryText 2"
@@ -26,8 +21,8 @@ if __name__ == '__main__':
         # encode to vectors sing s-bert
         print("performing feature extraction using s-bert")
         x, y = feature_extractor(data_dict,
-                                 backbone_to_use=args.backbone,
-                                 encode_headlines_separately=not args.dont_use_headlines)
+                                 backbone_to_use=backbone,
+                                 encode_headlines_separately=not dont_use_headlines)
 
         # find the best classifier
         print("performing model selection")
@@ -44,5 +39,15 @@ if __name__ == '__main__':
         pickle_out.close()
     else:
         run_flask()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_mode', action='store_true')
+    parser.add_argument('--backbone', default='all-mpnet-base-v2')
+    parser.add_argument('--dont_use_headlines',  action='store_false')
+    args = parser.parse_args()
+
+    main(train_mode=args.train_mode, backbone=args.backbone, dont_use_headlines=args.dont_use_headlines)
 
 
